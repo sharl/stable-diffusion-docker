ddc38939c77f4de310b4b932429b3f930a926062
Sharl Morlaroll <sharl@hauN.org>
2022-09-05 22:12:09 +0900

    add options

--- gen.py
+++ gen.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import sys
+import argparse
 import requests
 from urllib.parse import quote
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--seed', type=int, default=0)
+parser.add_argument('--steps', type=int, default=20)
+parser.add_argument('--timeout', type=int, default=30)
+parser.add_argument('args', nargs='+', default='')
+args = parser.parse_args()
 
-text = quote(' '.join(sys.argv[1:]))
-requests.get('http://localhost:8080/' + text, timeout=30)
+prompt = quote(' '.join(args.args))
+if args.seed:
+    requests.get('http://localhost:8080/{}/{}/{}'.format(prompt, args.steps, args.seed), timeout=args.timeout)
+else:
+    requests.get('http://localhost:8080/{}/{}'.format(prompt, args.steps), timeout=args.timeout)
