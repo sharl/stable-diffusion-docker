44f3b2cf41dda447ff616c170ae548424a87be45
Sharl Morlaroll <sharl@hauN.org>
2022-09-07 19:36:23 +0900

    remove NSFW image

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM tensorflow/tensorflow:latest-gpu
 
 RUN pip install diffusers pillow torch transformers \
   --extra-index-url https://download.pytorch.org/whl/cu116
-## RUN sed -i -e '/has_nsfw_concept in/,+9d' /usr/local/lib/python3.8/dist-packages/diffusers/pipelines/stable_diffusion/safety_checker.py
 RUN pip install bottle
 
 RUN useradd -m huggingface
--- build.sh
+++ build.sh
@@ -28,19 +28,10 @@ run() {
         "$CWD"
 }
 
-nsfw() {
-    mkdir -p output
-    docker run -d --rm --gpus=all -p 8080:8080 \
-        -v huggingface:/home/huggingface/.cache/huggingface \
-        -v "$PWD"/output:/home/huggingface/output \
-        "$CWD":NSFW
-}
-
 case ${1:-build} in
     build) build ;;
     clean) clean ;;
     dev) dev "$@" ;;
     run) run ;;
-    nsfw) nsfw ;;
     *) echo "$0: No command named '$1'" ;;
 esac
